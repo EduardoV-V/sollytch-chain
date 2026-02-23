ebce8e4b81ecb5cc68627376e9c94985b86f8e1c
EduardoV-V <valenteeduardo39@gmail.com>
2026-02-23 14:19:30 -0300

    Adicionando cliente benchmark

--- sollytch-chain/main.go
+++ sollytch-chain/main.go
@@ -496,6 +496,7 @@ func predictFromCSV(model *trees.ID3DecisionTree, target string, csvRow string) 
 	6) Cria uma chave composta para indexação por lote
 */
 func (s *SmartContract) StoreTest(ctx contractapi.TransactionContextInterface, testID string, jsonStr string, predictStr string) error {
+	start := time.Now()
 	// Verifica se já existe um teste com o mesmo ID
 	existing, err := ctx.GetStub().GetState(testID)
 	if err != nil {
@@ -587,6 +588,10 @@ func (s *SmartContract) StoreTest(ctx contractapi.TransactionContextInterface, t
 	}
 
 	// Armazena o indice no ledger
+	elapsed := time.Since(start).Seconds()
+	fmt.Printf("BENCHMARK_METRIC: { \"function\": \"StoreTest\", \"testId\": \"%s\", \"executionTime\": %.6f, \"timestamp\": \"%s\" }\n",
+		testID, elapsed, time.Now().Format(time.RFC3339Nano))
+
 	return ctx.GetStub().PutState(indexKey, []byte{0x00})
 }
 
@@ -675,6 +680,7 @@ func (s *SmartContract) GetTestsByLote(ctx contractapi.TransactionContextInterfa
 	com os modelos de Machine Learning, apenas atualiza o teste com a string json recebida
 */
 func (s *SmartContract) UpdateTest(ctx contractapi.TransactionContextInterface, testID string, fullJSON string) error {
+	start := time.Now()
 	// Busca o teste existente no ledger
 	existingBytes, err := ctx.GetStub().GetState(testID)
 	if err != nil {
@@ -747,6 +753,11 @@ func (s *SmartContract) UpdateTest(ctx contractapi.TransactionContextInterface, 
 	if err != nil {
 		return err
 	}
+
+	elapsed := time.Since(start).Seconds()
+
+	fmt.Printf("BENCHMARK_METRIC: { \"function\": \"UpdateTest\", \"testId\": \"%s\", \"executionTime\": %.6f, \"timestamp\": \"%s\" }\n", 
+        testID, elapsed, time.Now().Format(time.RFC3339Nano))
 
 	// Persiste o novo estado do teste no ledger
 	return ctx.GetStub().PutState(testID, bytes)
